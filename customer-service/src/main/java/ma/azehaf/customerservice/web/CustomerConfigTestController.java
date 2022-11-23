ae7b3825707377ea4feb5e12be74921ed39917dd
azehaf issam <issam.test@outlook.fr>
2022-11-23 19:19:38 +0100

    test atucator on params

--- customer-service/src/main/java/ma/azehaf/customerservice/web/CustomerConfigTestController.java
+++ customer-service/src/main/java/ma/azehaf/customerservice/web/CustomerConfigTestController.java
@@ -1,12 +1,14 @@
 package ma.azehaf.customerservice.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class CustomerConfigTestController {
     @Value("${global.params.p1}")
     private String p1;
