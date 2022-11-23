d36a6904d40eb89165162e26d3c2fca4bb8c60d4
azehaf issam <issam.test@outlook.fr>
2022-11-23 19:38:26 +0100

    creating customer-service classes and repositories

--- customer-service/src/main/java/ma/azehaf/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/azehaf/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,31 @@
 package ma.azehaf.customerservice;
 
+import ma.azehaf.customerservice.entities.Customer;
+import ma.azehaf.customerservice.repo.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.saveAll(List.of(
+                    Customer.builder().name("Issam").email("issam.azf@gmail.com").build(),
+                    Customer.builder().name("elhoucine").email("elhoucine.azf@gmail.com").build(),
+                    Customer.builder().name("Anouar").email("anouar.test@gmail.com").build()
+                    ));
+            customerRepository.findAll().forEach(System.out::println);
+        };
     }
 
 }
