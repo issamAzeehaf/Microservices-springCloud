af3d8c3162b4b5b22e78b31f8616a8730a4cfc10
azehaf issam <issam.test@outlook.fr>
2022-12-12 18:49:26 +0100

    adding inventory-service

--- inventory-service/src/main/java/ma/azehaf/inventoryservice/entities/Product.java
+++ inventory-service/src/main/java/ma/azehaf/inventoryservice/entities/Product.java
@@ -1,2 +1,21 @@
-package ma.azehaf.inventoryservice.entities;public class Product {
+package ma.azehaf.inventoryservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private int quantity;
 }
